db5a29ed65383a47c5bfb7ce915c21cdd4f42caa
kiwosu <dntjrgkrdnjs@gmail.com>
2023-11-10 14:12:28 +0900

    카카오 맵 만들기

---
+++ src/test/java/com/example/kakaomap/KakaoMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
